2c89d4a6907696bbb3cb54a9a47d1745abb4e888
Luis Felipe Garcia Herrera <luisfelipegarciacsj@gmail.com>
2020-08-16 00:46:02 -0500

    feat: add implementation initial version of store

--- CarritoDeCompra.py
+++ CarritoDeCompra.py
@@ -1,7 +1,15 @@
+import Item
+
 class CarritoDeCompra:
+    items = []
 
     def agregar_item(self, producto, cantidad):
+        self.items.append(Item(producto,cantidad))
 
-
-    def calcular_total():
-        p
+    def calcular_total(self):
+        total = 0
+        
+        for item in self.items:
+           total += item.calcular_total()
+    
+        return total
--- Tienda.py
+++ Tienda.py
@@ -1,9 +1,15 @@
 class Tienda:
     acumulado = ""
+    productos = []
+    clientes = []
 
     def __init__(self, acumulador):
         self.acumulador = acumulador
     
     def agregar_producto_a_carrito(self, producto, cantidad, cliente):
+        
+        cliente.agregar_producto_a_carrito(producto,cantidad)
+        
+        
         print('Producto', self.producto , ' agregado con ', self.cantidad, 'por el cliente ', self.cliente)
-                        
+                                                                            
